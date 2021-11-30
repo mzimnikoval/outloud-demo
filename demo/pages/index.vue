da54177a16a124e1b97971506631b1c4a6b9ab4e
Maros <maros.zimnikoval@gmail.com>
2021-11-30 20:31:31 +0100

    Finish 1 day

---
+++ demo/pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+	<main>
+		<section class="box-wrapper bg-white">
+			<h1>Registration Form</h1>
+			<form action="">
+				<div class="row">
+					<div class="col">
+						<div class="input-group">
+							<input type="email" name="email" id="">	
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<div class="input-group">
+							<input type="text" name="first_name" id="">
+						</div>
+					</div>
+					<div class="col">
+						<div class="input-group">
+							<input type="text" name="second_name" id="">
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<div class="input-group">
+							<select name="" id=""></select>	
+						</div>							
+					</div>
+					<div class="col">
+						<div class="input-group">
+							<select name="" id=""></select>	
+						</div>		
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<div class="input-group">
+							<input type="password" name="password" id="">
+						</div>
+					</div>
+					<div class="col">
+						<div class="input-group">
+							<input type="password" name="password-repeat" id="">
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<label for="">Private Profile</label>						
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<div class="separator"></div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<div class="flex">
+							<button type="submit" class="btn">Sign up</button>
+							<label class="checkbox bounce">
+								<input type="checkbox">
+								<span>Creating an account means you’re okay with our Privacy Policy.</span>
+							</label>
+						</div>
+					</div>
+				</div>
+			</form>
+		</section>
+	</main>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
